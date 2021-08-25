10062e28cf90296377293392663e42c631c7051f
ojcarlos <65193386+ojcarlos@users.noreply.github.com>
2021-08-25 18:58:14 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
+
+//swiper
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+//scrollReveal
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about.text,
+  #service .header, #service .cards,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .logo footer .social`,
+  { interval: 100 }
+)
+
+//back to top
+const backButton = document.querySelector('.back')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backButton.classList.add('show')
+  } else {
+    backButton.classList.remove('show')
+  }
+})
